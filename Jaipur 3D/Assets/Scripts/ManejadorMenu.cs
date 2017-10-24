f484cd39f0f6af5321f08020158e08455dfd0715
Christian Lugo <christianrlugo5@gmail.com>
2017-10-23 23:55:08 -0600

    Escena Menú
    -Se creó la escena menú en la cual contenemos las siguientes pantallas:
    -El menú principal, en el cual hay 2 botones para ir a los menús "crear
    partida" y "conectarse a partida"
    -El menú para crear partida, en el cual tenemos la pantalla de espera y
    el boton para cancelar la partida.
    -El menú para conectarse a una partida, en el cual introducimos la IP y
    el puerto de la partida creada, tenemos el boton para conectar o para ir
    al menú anterior.
    -Se creó la clase "ManejadorMenu" la cual nos permite alternar los
    distintos menús mencionados por medio de los botones.

---
+++ Jaipur 3D/Assets/Scripts/ManejadorMenu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class ManejadorMenu : MonoBehaviour {
+
+    public GameObject menuPrincipal;
+    public GameObject menuCrear;
+    public GameObject menuConectar;
+    
+
+    // Use this for initialization
+    void Start () {
+        OcultarTodos();
+        menuPrincipal.SetActive(true);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void MenuPrincipal()
+    {
+        OcultarTodos();
+        menuPrincipal.SetActive(true);
+    }
+
+    public void MenuCrear()
+    {
+        OcultarTodos();
+        menuCrear.SetActive(true);
+    }
+
+    public void MenuUnirse()
+    {
+        OcultarTodos();
+        menuConectar.SetActive(true);
+        
+    }
+
+    public void BotonConectarAlServidor()
+    {
+        
+        
+    }
+
+    void OcultarTodos()
+    {
+        menuPrincipal.SetActive(false);
+        menuConectar.SetActive(false);
+        menuCrear.SetActive(false);
+    }
+
+}
