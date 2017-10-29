d210e0425043cc0a78f8f5a9865ad6bd46b1e936
David Rochin <jdrc8@hotmail.com>
2017-10-29 16:08:34 -0700

    Serialización de Deserialización de Movimientos
    Se agregaron 2 metodos en Movimiento:
    -Un método para serializar los objetos de tipo Movimiento.
    -Un método estatico para deserializarlos.

--- Jaipur 3D/Assets/Clases/Movimiento.cs
+++ Jaipur 3D/Assets/Clases/Movimiento.cs
@@ -1,7 +1,27 @@
-﻿
+﻿using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
 public class Movimiento {
     public int[] ids;
     public TipoMovimiento tipoMovimiento;
 
-    public enum TipoMovimiento { Tomar, Vender, Trueque, RevolverCartas }
+    public enum TipoMovimiento { Ninguno, Tomar, Vender, Trueque, RevolverCartas }
+
+    public Movimiento(TipoMovimiento tipoMov) {
+        tipoMovimiento = tipoMov;
+    }
+
+    public byte[] Serializar() {
+        MemoryStream stream = new MemoryStream();
+        BinaryFormatter binaryFormatter = new BinaryFormatter();
+        binaryFormatter.Serialize(stream, this);
+        return stream.GetBuffer();
+    }
+
+    public static Movimiento Deserializar(byte[] datos) {
+        MemoryStream stream = new MemoryStream(datos);
+        BinaryFormatter binaryFormatter = new BinaryFormatter();
+        return (Movimiento)binaryFormatter.Deserialize(stream);
+    }
 }
